e6ea4e71fd980c779048a52331ace4486c0999f4
SangSonyratt <sangsonyrath17@kit.edu.kh>
2018-06-11 11:39:39 +0700

    jkasjkjds

---
+++ whileloop.scala
@@ -0,0 +1,18 @@
+object mainobj
+{
+    def main(args:Array[String]): Unit=
+    {
+        var a=0
+        while(a<=20)
+        {
+            println(a);
+            a=a+2;
+        }
+        var b=1
+        while(true)
+        {
+            println(a)
+            b=b+2;
+        }
+    }
+}
